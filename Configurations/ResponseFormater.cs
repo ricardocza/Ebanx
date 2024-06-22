ac1c70fccef79beb76d4fb7fe25b9b28b13774e8
ricardocza <ricardovcza@gmail.com>
2024-06-22 10:55:03 -0300

    feat: adjust response for the test

---
+++ Configurations/ResponseFormater.cs
@@ -0,0 +1,35 @@
+﻿using Ebanx.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Collections;
+using System.Text;
+
+namespace Ebanx.Configurations;
+
+public class ResponseFormater : TextOutputFormatter
+{
+    public ResponseFormater()
+    {
+        SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/json"));
+        SupportedEncodings.Add(Encoding.UTF8);
+        SupportedEncodings.Add(Encoding.Unicode);
+    }
+
+    public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+    {
+        var response = context.HttpContext.Response;
+
+        var buffer = new StringBuilder();        
+        var data = context.Object as ResponseDto;
+        if (data != null)
+        {
+            buffer.Append(data?.ToString());
+        }
+        else
+        {
+            buffer.Append(context.Object);
+        }
+
+        await response.WriteAsync(buffer.ToString());
+    }
+}
---
+++ Models/ResponseDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Ebanx.Models;
+
+public class ResponseDto
+{
+    public AccountDto? Destination { get; set; }
+    public AccountDto? Origin { get; set; }
+
+    public override string ToString()
+    {
+        if (Destination == null)
+        {
+            return $"{{\"origin\": {Origin}}}";
+        }
+        else if (Origin == null)
+        {
+            return $"{{\"destination\": {Destination}}}";
+        }
+        else
+        {
+            return $"{{\"origin\": {Origin}, \"destination\": {Destination}}}";
+        }
+    }
+}
